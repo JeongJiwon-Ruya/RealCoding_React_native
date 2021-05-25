14d82e23d9f3cc83692824754d0c21c20bee125b
201702000 <jundol1222@naver.com>
2021-05-26 00:16:43 +0900

    04. Button and arrow function

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style = {[styles.textBig, styles.textRed]}>Hello, World!</Text>
+      <Button title={'Click this'} onPress={() => console.log('Clicked')}/>
       <StatusBar style="auto" />
     </View>
   );
