d097358ecc91b3f34461dd7f2080d7235f8b943f
201702000 <jundol1222@naver.com>
2021-05-31 00:09:30 +0900

    11. width and align

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ class CounterButton extends React.Component {
     };
 
     return (
-      <View style={styles.container}>
+      <View style={styles.buttonContainer}>
         <Text>{this.state.counter}</Text>
         <Button title={'click me!'} onPress={clickHandler}/>
         <StatusBar style="auto" />
@@ -53,10 +53,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textBig: {
-    fontSize: 32,
-  },
-  textRed: {
-    color: 'red',
+  buttonContainer: {
+    alignItems: 'center',
+    width: 300,
   },
 });
