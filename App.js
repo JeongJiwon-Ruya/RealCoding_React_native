64496c46eb07f1960eaaeb63254e6a4052cfc8e3
JeongJiwon <eetr3295@gmail.com>
2021-05-31 09:10:30 +0900

    19. ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView } from 'react-native';
 
 
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,13 +8,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.fullscreen}>
-        <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-              width: 128,
-              height: 128,
-            }} />
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
           <Text style={{fontSize:96}}>Scroll me plz</Text>
           <Image source={{
               uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
@@ -45,18 +40,21 @@ export default class App extends React.Component {
               width: 128,
               height: 128,
             }} />
-          <StatusBar style="auto" />
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+              width: 128,
+              height: 128,
+            }} />
+        </ScrollView>
+        <StatusBar style="auto" />
       </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  fullscreen: {
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    borderColor:'red',
-    borderWidth: 2,
-    alignItems: 'center',
   },
 });
