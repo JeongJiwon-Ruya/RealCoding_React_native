83b878e014e8ec7e65ca7d252405e3798de5d751
Bak Sanggyun <baksanggyun2856@gmail.com>
2021-06-01 21:52:18 +0900

    37

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -1,22 +1,65 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+
+const API_KEY = '{7ae5bd08b5e81c46c8f9b5feeb2a9f96}';
+const queryUrl = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
 
 export default class WeatherDetailScreen extends React.Component {
   static navigationOptions = {
     title: 'Weather Information',
   };
 
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      main: null
+    };
+  }
+
+  componentDidMount() {
     const {
       route: {
         params: { city },
       },
     } = this.props;
 
-    console.log('route = ', city);
+    fetch(queryUrl(city))
+      .then(response => response.json())
+      .then(info => {
+        console.log(info);
+        this.setState({
+          ...info,
+          isLoading: false,
+          main: info
+        });
+      });
+  }
+
+  render() {
+    const {
+        route: {
+          params: { city },
+        },
+        navigation,
+      } = this.props;
+  
+      navigation.setOptions({ title: `Weather Information: ${city}` });
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <Text>데이터를 불러오는 중입니다.</Text>
+        </View>
+      )
+    }
+
+    let celsius = this.state.main.temp - 273.15;
+
 
     return (
       <View style={styles.container}>
+          <Text>온도: {celsius.toFixed(1)}</Text>
       </View>
     );
   }
